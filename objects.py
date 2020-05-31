5729cd8677577e6bbc8fd02fb1d21cde4717012b
Arturo Torres <arturotc22@gmail.com>
2020-05-30 22:18:16 -0500

    Fix textures

--- objects.py
+++ objects.py
@@ -1,305 +1,308 @@
 # Attributes for Wolf Model
 wolfAtt = {
-  "head" : {
-    "position": (0,0,0),
-    "size": (1,1,1),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "nose": {
-    "position": (0,-0.5,1.3),
-    "size": (0.5,0.5,0.5),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "body": {
-    "position": (0,0.1,-2),
-    "size": (1.2,1,2),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "tail": {
-    "position": (0,-0.2,-4),
-    "size": (0.2,0.2,1),
-    "rotation": (-45,1,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "leg_fr": {
-    "position": (0.5,-1.5,-0.8),
-    "size": (0.2,1,0.2),
-    "rotation": (45,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "leg_fl": {
-    "position": (-0.5,-1.5,-0.4),
-    "size": (0.2,1,0.2),
-    "rotation": (-45,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "leg_br": {
-    "position": (0.5,-1.5,-2),
-    "size": (0.2,1,0.2),
-    "rotation": (-45,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "leg_bl": {
-    "position": (-0.5,-1.5,-3),
-    "size": (0.2,1,0.2),
-    "rotation": (45,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
+    "head": {
+        "position": (0, 0, 0),
+        "size": (1, 1, 1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "nose": {
+        "position": (0, -0.5, 1.3),
+        "size": (0.5, 0.5, 0.5),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "body": {
+        "position": (0, 0.1, -2),
+        "size": (1.2, 1, 2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "tail": {
+        "position": (0, -0.2, -4),
+        "size": (0.2, 0.2, 1),
+        "rotation": (-45, 1, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "leg_fr": {
+        "position": (0.5, -1.5, -0.8),
+        "size": (0.2, 1, 0.2),
+        "rotation": (45, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "leg_fl": {
+        "position": (-0.5, -1.5, -0.4),
+        "size": (0.2, 1, 0.2),
+        "rotation": (-45, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "leg_br": {
+        "position": (0.5, -1.5, -2),
+        "size": (0.2, 1, 0.2),
+        "rotation": (-45, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "leg_bl": {
+        "position": (-0.5, -1.5, -3),
+        "size": (0.2, 1, 0.2),
+        "rotation": (45, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
 }
 
 # Attributes for Steve Model
 steveAtt = {
-  "head" : {
-    "position": (0,1.6,0),
-    "size": (0.6,0.6,0.6),
-    "rotation": (0,0,0,0),
-    "color": (0.98,0.92,0.81)
-  },
-  "body": {
-    "position": (0,0,0),
-    "size": (0.75,1,0.3),
-    "rotation": (0,0,0,0),
-    "color": (0.25,0.79,0.96)
-  },
-  "left_arm_skin": {
-    "position": (-1,0,-0.5),
-    "size": (0.35,1,0.3),
-    "rotation": (45,1,0,0),
-    "color": (0.25,0.79,0.96)
-  },
-  "right_arm_skin": {
-    "position": (1,0,0.5),
-    "size": (0.35,1,0.3),
-    "rotation": (-45,1,0,0),
-    "color": (0.25,0.79,0.96)
-  },
-  "right_leg": {
-    "position": (0.25,-2,-0.5),
-    "size": (0.375,1.2,0.3),
-    "rotation": (45,0,0,0),
-    "color": (0.11, 0.40, 1)
-  },
-  "left_leg": {
-    "position": (-0.25,-2,0.5),
-    "size": (0.375,1.2,0.3),
-    "rotation": (-45,0,0,0),
-    "color": (0.11, 0.40, 1)
-  },
+    "head": {
+        "position": (0, 1.6, 0),
+        "size": (0.6, 0.6, 0.6),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.98, 0.92, 0.81),
+        "texture": "./textures/steve_face.png"
+    },
+    "body": {
+        "position": (0, 0, 0),
+        "size": (0.75, 1, 0.3),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.25, 0.79, 0.96),
+        "texture": "./textures/blue_pants.jpeg"
+    },
+    "left_arm_skin": {
+        "position": (-1, 0, -0.5),
+        "size": (0.35, 1, 0.3),
+        "rotation": (45, 1, 0, 0),
+        "color": (0.25, 0.79, 0.96)
+    },
+    "right_arm_skin": {
+        "position": (1, 0, 0.5),
+        "size": (0.35, 1, 0.3),
+        "rotation": (-45, 1, 0, 0),
+        "color": (0.25, 0.79, 0.96)
+    },
+    "right_leg": {
+        "position": (0.25, -2, -0.5),
+        "size": (0.375, 1.2, 0.3),
+        "rotation": (45, 0, 0, 0),
+        "color": (0.11, 0.40, 1)
+    },
+    "left_leg": {
+        "position": (-0.25, -2, 0.5),
+        "size": (0.375, 1.2, 0.3),
+        "rotation": (-45, 0, 0, 0),
+        "color": (0.11, 0.40, 1)
+    },
 }
 
 # Attributes for Tree Model
 treeAtt = {
-  "trunk": {
-    "position": (0,0,0),
-    "size": (1,3,1),
-    "rotation": (0,0,0,0),
-    "color": (0.41,0.25,0.2)
-  },
-   "leaves_lower": {
-    "position": (0,3,0),
-    "size": (4,2,4),
-    "rotation": (0,0,0,0),
-    "color": (0.075,0.50,0.059)
-  },
-   "leaves_middle": {
-    "position": (0,5,0),
-    "size": (3,1,3),
-    "rotation": (0,0,0,0),
-    "color": (0.075,0.50,0.059)
-  },
-   "leaves_upper1": {
-    "position": (0,6,0),
-    "size": (2,1,1),
-    "rotation": (0,0,0,0),
-    "color": (0.075,0.50,0.059)
-  },
-   "leaves_upper2": {
-    "position": (0,6,0),
-    "size": (1,1,2),
-    "rotation": (0,0,0,0),
-    "color": (0.075,0.50,0.059)
-  }
+    "trunk": {
+        "position": (0, 0, 0),
+        "size": (1, 3, 1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.41, 0.25, 0.2)
+    },
+    "leaves_lower": {
+        "position": (0, 3, 0),
+        "size": (4, 2, 4),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.075, 0.50, 0.059)
+    },
+    "leaves_middle": {
+        "position": (0, 5, 0),
+        "size": (3, 1, 3),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.075, 0.50, 0.059)
+    },
+    "leaves_upper1": {
+        "position": (0, 6, 0),
+        "size": (2, 1, 1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.075, 0.50, 0.059)
+    },
+    "leaves_upper2": {
+        "position": (0, 6, 0),
+        "size": (1, 1, 2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.075, 0.50, 0.059)
+    }
 }
 
 # Attributes for Mountain Model
 mountainAtt = {
-  "floor_lower1": {
-    "position": (0,-2,-35),
-    "size": (20,1,20),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_lower2": {
-    "position": (-20,-2,-25),
-    "size": (7,1,30),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_middle1": {
-    "position": (0,-1,-37),
-    "size": (18,1,18),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_middle2": {
-    "position": (-21,-1,-27),
-    "size": (5,1,28),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_middle3": {
-    "position": (0,0,-39),
-    "size": (16,1,16),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_middle4": {
-    "position": (-21,0,-30),
-    "size": (5,1,25),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_upper1": {
-    "position": (7,1,-39),
-    "size": (8,2,10),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_upper2": {
-    "position": (-21,1,-35),
-    "size": (5,1,20),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_upper3": {
-    "position": (-21,2,-43),
-    "size": (4,2,10),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  },
-  "floor_upper4": {
-    "position": (-21,4,-43),
-    "size": (3,2,7),
-    "rotation": (0,0,0,0),
-    "color": (0.54,0.37,0.27)
-  }
+    "floor_lower1": {
+        "position": (0, -2, -35),
+        "size": (20, 1, 20),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_lower2": {
+        "position": (-20, -2, -25),
+        "size": (7, 1, 30),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_middle1": {
+        "position": (0, -1, -37),
+        "size": (18, 1, 18),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_middle2": {
+        "position": (-21, -1, -27),
+        "size": (5, 1, 28),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_middle3": {
+        "position": (0, 0, -39),
+        "size": (16, 1, 16),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_middle4": {
+        "position": (-21, 0, -30),
+        "size": (5, 1, 25),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_upper1": {
+        "position": (7, 1, -39),
+        "size": (8, 2, 10),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_upper2": {
+        "position": (-21, 1, -35),
+        "size": (5, 1, 20),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_upper3": {
+        "position": (-21, 2, -43),
+        "size": (4, 2, 10),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    },
+    "floor_upper4": {
+        "position": (-21, 4, -43),
+        "size": (3, 2, 7),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.54, 0.37, 0.27)
+    }
 }
 
 # Attributes for Pig Model
 pigAtt = {
-  "head" : {
-    "position": (0,0,0),
-    "size": (0.6,0.6,0.6),
-    "rotation": (0,0,0,0),
-    "color": (0.98,0.63, 0.58)
-  },
-  "nose" : {
-    "position": (0,-0.2,0.7),
-    "size": (0.3,0.2,0.1),
-    "rotation": (0,0,0,0),
-    "color": (0.98,0.75,0.72)
-  },
-  "body": {
-    "position": (0,-0.1,-1),
-    "size": (0.8,0.5,1),
-    "rotation": (0,0,0,0),
-    "color": (0.98,0.63, 0.58)
-  },
-  "leg_fl": {
-    "position": (-0.4,-0.8,-0.3),
-    "size": (0.3,0.5,0.3),
-    "rotation": (45,0,0,0),
-    "color": (0.98,0.63, 0.58)
-  },
-  "leg_fr": {
-    "position": (0.4,-0.8,-0.1),
-    "size": (0.3,0.5,0.3),
-    "rotation": (-45,0,0,0),
-    "color": (0.98,0.75,0.72)
-  },
-  "leg_bl": {
-    "position": (-0.4,-0.8,-1.3),
-    "size": (0.3,0.5,0.3),
-    "rotation": (-45,0,0,0),
-    "color": (0.98,0.75,0.72)
-  },
-  "leg_br": {
-    "position": (0.4,-0.8,-1.5),
-    "size": (0.3,0.5,0.3),
-    "rotation": (45,0,0,0),
-    "color": (0.98,0.63, 0.58)
-  }
+    "head": {
+        "position": (0, 0, 0),
+        "size": (0.6, 0.6, 0.6),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.98, 0.63, 0.58),
+        "texture": "./textures/pink_text.jpeg"
+    },
+    "nose": {
+        "position": (0, -0.2, 0.7),
+        "size": (0.3, 0.2, 0.1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.98, 0.75, 0.72)
+    },
+    "body": {
+        "position": (0, -0.1, -1),
+        "size": (0.8, 0.5, 1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.98, 0.63, 0.58)
+    },
+    "leg_fl": {
+        "position": (-0.4, -0.8, -0.3),
+        "size": (0.3, 0.5, 0.3),
+        "rotation": (45, 0, 0, 0),
+        "color": (0.98, 0.63, 0.58)
+    },
+    "leg_fr": {
+        "position": (0.4, -0.8, -0.1),
+        "size": (0.3, 0.5, 0.3),
+        "rotation": (-45, 0, 0, 0),
+        "color": (0.98, 0.75, 0.72)
+    },
+    "leg_bl": {
+        "position": (-0.4, -0.8, -1.3),
+        "size": (0.3, 0.5, 0.3),
+        "rotation": (-45, 0, 0, 0),
+        "color": (0.98, 0.75, 0.72)
+    },
+    "leg_br": {
+        "position": (0.4, -0.8, -1.5),
+        "size": (0.3, 0.5, 0.3),
+        "rotation": (45, 0, 0, 0),
+        "color": (0.98, 0.63, 0.58)
+    }
 }
 
 # Attributes for Skeleton Model
 skeletonAtt = {
-  "head" : {
-    "position": (0,1.4,0),
-    "size": (0.6,0.6,0.6),
-    "rotation": (0,0,0,0),
-    "color": (0.85, 0.85, 0.85)
-  },
-  "body": {
-    "position": (0,0.5,0),
-    "size": (0.75,0.5,0.2),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "column": {
-    "position": (0,-0.5,-0.1),
-    "size": (0.2,0.5,0.05),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "rib": {
-    "position": (0,-0.3,0),
-    "size": (0.5,0.1,0.05),
-    "rotation": (0,0,0,0),
-    "color": (0.82, 0.82, 0.82)
-  },
-  "ribl": {
-    "position": (-0.4,-0.2,0),
-    "size": (0.1,0.2,0.1),
-    "rotation": (0,0,0,0),
-    "color": (0.82, 0.82, 0.82)
-  },
-  "ribr": {
-    "position": (0.4,-0.2,0),
-    "size": (0.1,0.2,0.1),
-    "rotation": (0,0,0,0),
-    "color": (0.82, 0.82, 0.82)
-  },
-  "hip": {
-    "position": (0,-1,0.1),
-    "size": (0.6,0.1,0.3),
-    "rotation": (0,0,0,0),
-    "color": (0.90, 0.90, 0.90)
-  },
-  "left_arm_skin": {
-    "position": (-0.8,0,0),
-    "size": (0.2,1,0.2),
-    "rotation": (0,0,0,0),
-    "color": (0.85, 0.85, 0.85)
-  },
-  "right_arm_skin": {
-    "position": (0.8,0,0),
-    "size": (0.2,1,0.2),
-    "rotation": (0,0,0,0),
-    "color": (0.85, 0.85, 0.85)
-  },
-  "right_leg": {
-    "position": (0.35,-2,0),
-    "size": (0.2,1,0.2),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
-  "left_leg": {
-    "position": (-0.35,-2,0),
-    "size": (0.2,1,0.2),
-    "rotation": (0,0,0,0),
-    "color": (0.87, 0.87, 0.87)
-  },
+    "head": {
+        "position": (0, 1.4, 0),
+        "size": (0.6, 0.6, 0.6),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.85, 0.85, 0.85)
+    },
+    "body": {
+        "position": (0, 0.5, 0),
+        "size": (0.75, 0.5, 0.2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "column": {
+        "position": (0, -0.5, -0.1),
+        "size": (0.2, 0.5, 0.05),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "rib": {
+        "position": (0, -0.3, 0),
+        "size": (0.5, 0.1, 0.05),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.82, 0.82, 0.82)
+    },
+    "ribl": {
+        "position": (-0.4, -0.2, 0),
+        "size": (0.1, 0.2, 0.1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.82, 0.82, 0.82)
+    },
+    "ribr": {
+        "position": (0.4, -0.2, 0),
+        "size": (0.1, 0.2, 0.1),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.82, 0.82, 0.82)
+    },
+    "hip": {
+        "position": (0, -1, 0.1),
+        "size": (0.6, 0.1, 0.3),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.90, 0.90, 0.90)
+    },
+    "left_arm_skin": {
+        "position": (-0.8, 0, 0),
+        "size": (0.2, 1, 0.2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.85, 0.85, 0.85)
+    },
+    "right_arm_skin": {
+        "position": (0.8, 0, 0),
+        "size": (0.2, 1, 0.2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.85, 0.85, 0.85)
+    },
+    "right_leg": {
+        "position": (0.35, -2, 0),
+        "size": (0.2, 1, 0.2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
+    "left_leg": {
+        "position": (-0.35, -2, 0),
+        "size": (0.2, 1, 0.2),
+        "rotation": (0, 0, 0, 0),
+        "color": (0.87, 0.87, 0.87)
+    },
 }
--- wolf.py
+++ wolf.py
@@ -7,70 +7,109 @@
 # Jorge Arturo Torres A01176590
 
 
-
 import pygame
 from pygame.locals import *
 from math import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from PIL import Image
 
 from constants import *
+import numpy as np
+
 
 class Object3D:
-  def __init__(self, parts):
-    self.parts = parts
-    self.vertsTmp = {}
-    self.facesTmp = {}
-    for value, part in self.parts.items():
-      self.vertsTmp[value] = self.set_vertices(part["position"],part["size"],part["rotation"][0])
-      size = len(self.vertsTmp[value]) - 8
-      self.facesTmp[value] = []
-      for face in surfaces:
-        newface = []
-        for ang in face:
-          newface.append(ang + size)
-        self.facesTmp[value].append(tuple(newface))
+    def __init__(self, parts, filename=""):
+        self.img = None
+        if filename is not "":
+            self.img = Image.open(filename)
+            self.img = self.img.convert("RGB")
+            self.img_data = np.array(list(self.img.getdata()), np.int8)
+        self.parts = parts
+        self.vertsTmp = {}
+        self.facesTmp = {}
+        self.__text_flags = {}
+        for value, part in self.parts.items():
+            self.vertsTmp[value] = self.set_vertices(
+                part["position"], part["size"], part["rotation"][0])
+            size = len(self.vertsTmp[value]) - 8
+            self.facesTmp[value] = []
+            for face in surfaces:
+                newface = []
+                for ang in face:
+                    newface.append(ang + size)
+                self.facesTmp[value].append(tuple(newface))
 
-  def set_vertices(self, position, size, r):
-      new_vertices = []
+    def set_vertices(self, position, size, r):
+        new_vertices = []
 
-      for vert in vertices:
-          new_vert = []
+        for vert in vertices:
+            new_vert = []
 
-          vert_x = vert[0]
-          vert_y = vert[1]
-          vert_z = vert[2]
+            vert_x = vert[0]
+            vert_y = vert[1]
+            vert_z = vert[2]
 
-          new_x = vert_x * size[0]
-          new_y = vert_y * size[1]
-          new_z = vert_z * size[2]
+            new_x = vert_x * size[0]
+            new_y = vert_y * size[1]
+            new_z = vert_z * size[2]
 
-          new_vert.append(new_x)
-          new_vert.append(new_y)
-          new_vert.append(new_z)
-          new_vertices.append(tuple(new_vert))
+            new_vert.append(new_x)
+            new_vert.append(new_y)
+            new_vert.append(new_z)
+            new_vertices.append(tuple(new_vert))
 
-      return new_vertices
+        return new_vertices
 
-  def run(self, position, rotation, scale):
-      glPushMatrix()
-      if rotation > 1:
-        glRotate(rotation,0,1,0)
-      if scale > 1:
-        print(scale)
-        glScaled(scale, scale, scale)
-      glTranslate(position[0],position[1],position[2])
-      for value, part in self.parts.items():
+    def run(self, position, rotation, scale):
         glPushMatrix()
-        glColor(part["color"])
-        glTranslate(part["position"][0],part["position"][1],part["position"][2])
-        if part["rotation"][0] != 0:
-          glRotate(part["rotation"][0],part["rotation"][1],part["rotation"][2],part["rotation"][3])
-        glBegin(GL_QUADS)
-        for face in self.facesTmp[value]:
-            for ver in face:
-                glVertex3fv(self.vertsTmp[value][ver]) 
-        glEnd()
+        if rotation > 1:
+            glRotate(rotation, 0, 1, 0)
+        if scale > 1:
+            print(scale)
+            glScaled(scale, scale, scale)
+        glTranslate(position[0], position[1], position[2])
+        for value, part in self.parts.items():
+            if 'texture' in part.keys():
+                print(value)
+                self.__text_flags[value] = True
+                self.read_texture()
+                glEnable(GL_TEXTURE_2D)
+                glBindTexture(GL_TEXTURE_2D, self.texture_id[0])
+                qobj = gluNewQuadric()
+                gluQuadricTexture(qobj, GL_TRUE)
+                glEnable(GL_TEXTURE_GEN_S)
+                glEnable(GL_TEXTURE_GEN_T)
+
+            glPushMatrix()
+            glColor(part["color"])
+            glTranslate(part["position"][0], part["position"]
+                        [1], part["position"][2])
+            if part["rotation"][0] != 0:
+                glRotate(part["rotation"][0], part["rotation"][1],
+                         part["rotation"][2], part["rotation"][3])
+            glBegin(GL_QUADS)
+            for face in self.facesTmp[value]:
+                for ver in face:
+                    glVertex3fv(self.vertsTmp[value][ver])
+            glEnd()
+            glPopMatrix()
         glPopMatrix()
-      glPopMatrix()
+
+    def read_texture(self):
+        self.texture_id = [0] * 1
+        glGenTextures(1, self.texture_id)
+        print(self.texture_id)
+        glBindTexture(GL_TEXTURE_2D, self.texture_id[0])
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                     self.img.size[0], self.img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, self.img_data)
+        glDisable(GL_TEXTURE_2D)
